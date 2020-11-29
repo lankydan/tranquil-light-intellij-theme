590974949294cc19e323881541465a80e08f9ca9
GitHub Action <action@github.com>
2020-11-29 22:10:56 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/lankydan/palenightintellijtheme/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.lankydan.palenightintellijtheme.services
+
+import com.github.lankydan.palenightintellijtheme.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/lankydan/palenightintellijtheme/services/MyProjectService.kt
+++ src/main/kotlin/com/github/lankydan/palenightintellijtheme/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.lankydan.palenightintellijtheme.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lankydan.palenightintellijtheme.MyBundle
 
 class MyProjectService(project: Project) {
 
