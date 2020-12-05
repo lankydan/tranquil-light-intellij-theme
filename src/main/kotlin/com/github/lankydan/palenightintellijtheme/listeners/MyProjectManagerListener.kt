c32ce09e0d80f390673845ce2093c187b75d7e83
LankyDan <danknewton@hotmail.com>
2020-12-05 22:03:48 +0000

    KLint why you do this!

--- src/main/kotlin/com/github/lankydan/palenightintellijtheme/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/lankydan/palenightintellijtheme/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
 package com.github.lankydan.palenightintellijtheme.listeners
 
+import com.github.lankydan.palenightintellijtheme.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import com.github.lankydan.palenightintellijtheme.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
